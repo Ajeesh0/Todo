4ec17c2d8766977be08815d89082a641788ff1be
Ajeesh <ajeesh0701@gmail>
2025-09-01 20:39:51 +0530

    added tasks

--- myapp/views.py
+++ myapp/views.py
@@ -1,6 +1,13 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,HttpResponse,redirect
+from .models import Task
 # Create your views here.
 
 def home(request):
-    return render (request,'home.html')
+    tasks=Task.objects.filter(is_completed=False).order_by('-updated_at')
+    
+    return render (request,'home.html',{"tasks":tasks})
+
+def addtask(request):
+    task=request.POST['task']
+    Task.objects.create(task=task)
+    return redirect('home')
